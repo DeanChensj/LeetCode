d08a5e9b885322139381ae28785d88d40581a462
Dean Chen <463575680@qq.com>
2016-04-10 00:40:14 +0800

    Group Anagrams: Hash, sort string.

--- Medium/049_Group-Anagrams.cpp
+++ Medium/049_Group-Anagrams.cpp
@@ -6,21 +6,21 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
     sort(strs.begin(), strs.end());
     
     int cnt = 0;
-    //            hash_code   index
     unordered_map<string, int> hash;
     for (int i = 0; i < strs.size(); i++) {
         int *tmp = new int[26];
         memset(tmp, 0, 26*4);
         for (int j = 0; j < strs[i].length(); j++) tmp[strs[i][j] - 'a']++;
         
-        string item = "";
+        char *cs = new char[104]; 
         for (int i = 0; i < 26; i++) {
-            item += (char)(tmp[i] & 0xff); 
-            item += (char)((tmp[i] >> 8) & 0xff); 
-            item += (char)((tmp[i] >> 16) & 0xff); 
-            item += (char)((tmp[i] >> 24) & 0xff);
+            cs[i*4] = tmp[i]; 
+            cs[i*4+1] = tmp[i] >>= 4; 
+            cs[i*4+2] = tmp[i] >>= 4; 
+            cs[i*4+3] = tmp[i] >>= 4;
         }
-        
+        string item(cs, 104);
+    
         if (hash.find(item) == hash.end()) {
             hash[item] = cnt++;
             vector<string> item{strs[i]};
@@ -29,4 +29,3 @@ vector<vector<string>> groupAnagrams(vector<string>& strs) {
     }
     return res;
 }
-
